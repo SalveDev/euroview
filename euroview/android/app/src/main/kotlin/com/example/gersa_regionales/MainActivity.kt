06f6f13783f3b2872922e94f43ddc2863b84a322
SalveDev <salvador.vera@gersamex.com.mx>
2025-04-30 12:39:52 -0600

    Remove Windows-specific files and configurations for the euroview project
    - Deleted .gitignore file for the Windows directory.
    - Removed CMakeLists.txt files for project and Flutter library configurations.
    - Eliminated generated plugin registrant files and associated headers.
    - Cleared out runner-related files including main application logic, resources, and utility functions.
    - Removed the Win32 window management implementation and its header.
    - Cleaned up all related resources and icons.

--- euroview/android/app/src/main/kotlin/com/example/gersa_regionales/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gersa_regionales
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- euroview/lib/Models/userModel.dart
+++
@@ -1,5 +0,0 @@
-class User {
-  static String id = "";
-  static String nombre = "";
-  static List<dynamic> roles = [];
-}
